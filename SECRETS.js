34662569c037178d5679bb6fefda9ddc2f1fe685
mksama1105 <mksama@gmail.com>
2025-06-30 22:51:10 +0700

    Update SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -5,7 +5,7 @@ const APP_KEY = "2GF5EFCvLkplZoMxC54vhx98y";
 const APP_SECRET = "M75BBcdgTcVMsl29kXPzE8YrNrBtnqmOyRK5BgfP7nbVhWKsqE";
 const ACCESS_TOKEN = "1939256842296967168-powjqoNXug1XOWokAGg13OqzUyvady";
 const ACCESS_SECRET = "7K7nE5fxkfYKFrSExqb1fDCwbK78MhVdleO0gSAPXwMn8";
-const GEMINI_API_KEY = "AIzaSyAP-dlJzd2uiJDevOYnDkYQzqH6VBrkVYo";
+const GEMINI_API_KEY = "AIzaSyA1aRNxXveWWprEF83YjQMkz7rZ71pPg78";
 
 const SECRETS = {
   APP_KEY,
